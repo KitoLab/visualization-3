86e3188678408c1b986492fbcfbfb7463d3dfeeb
Toshihiko Doi <36196747+ishot-l@users.noreply.github.com>
2019-02-13 14:44:47 +0900

    readChat.jsで変化したデータにしたがってグラフを描きかえるスクリプト

--- graphEvent.js
+++ graphEvent.js
@@ -1,4 +1,17 @@
 // クリックしているやつとそれに接続されているやつ以外にhiddenクラスを付ける
 function nodeClickEvent(d){
-	console.log(d.id);
+	information.text(get_label_from_id(d.id));
+
+	// 相手表示
+
+	conneted_nodes = links.filter(value => value.source.id == d.id || value.target.id == d.id);
+	console.log(conneted_nodes);
+
+	output_text = "";
+
+	conneted_nodes.sort(function(a, b) {
+		if (a.count > b.count) return 1;
+		if (b.count > a.count) return -1;
+		return 0;
+	})
 };
